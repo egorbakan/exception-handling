48463a500e91c4dca69345e99cbb09a1e617cbea
egorbakan <118420911+egorbakan@users.noreply.github.com>
2022-11-22 13:56:33 +0900

    Add files via upload

---
+++ Обработка исключений/Обработка исключений/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Обработка_исключений
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float a, b, deg;
+               
+            try
+            {
+                Console.WriteLine("Введите значение a");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите значение b");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                deg = (float)Math.Pow(a,b);
+                Console.WriteLine("Ответ = " +deg);
+            }
+
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+        }
+    }
+}
